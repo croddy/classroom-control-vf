7781bea85ca9cd3debce330ef34e34503ba3f561
nestlerw <bill@learningtreelabs.com>
2017-06-20 16:32:52 -0500

    No comma after include statement.

--- manifests/site.pp
+++ manifests/site.pp
@@ -42,8 +42,8 @@ node default {
   # This is where you can declare classes for all nodes.
   # Example:
   #   class { 'my_class': }
-  include role::classroom,
-  include users,
+  include role::classroom
+  include users
 }
 
 # Manage '/etc/motd'
