655b75c73a3f9d0f79e300766e260068100ce6f0
Ben Deibert <backpackingat@gmail.com>
2017-06-21 13:57:12 -0400

    removed trailing '}'

--- manifests/site.pp
+++ manifests/site.pp
@@ -37,4 +37,4 @@ ini_setting { 'random ordering':
 # definition. If there are no other nodes in this file, classes declared here
 # will be included in every node's catalog, *in addition* to any classes
 # specified in the console for that node.
-}
+
