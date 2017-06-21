2f22f945a0ca3f8bb3e7c45d992c1aea6e8dbc34
keeckle <kaleb_eckles@anfcorp.com>
2017-06-21 09:36:33 -0400

    commented out a bracket

--- manifests/site.pp
+++ manifests/site.pp
@@ -45,12 +45,12 @@ node default {
   include role::classroom
   
   # file { '/etc/motd':
-  # ensure => file,
-  # owner => 'root',
-  # group => 'root',
-  # mode => '0644',
-  # content => "I learned the basics of navigating a simple puppet infrastructure\n",
-  }
+    # ensure => file,
+    # owner => 'root',
+    # group => 'root',
+    # mode => '0644',
+    # content => "I learned the basics of navigating a simple puppet infrastructure\n",
+  #}
   
   exec { 'generate /etc/motd':
     command => 'cowsay "Welcome to ${::fqdn}!" > /etc/motd',
