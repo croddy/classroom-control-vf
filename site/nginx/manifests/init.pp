e7662af9763829dc18718b8ffce8038132e41d58
keeckle <kaleb_eckles@anfcorp.com>
2017-06-21 11:38:26 -0400

    add relationships

--- site/nginx/manifests/init.pp
+++ site/nginx/manifests/init.pp
@@ -6,11 +6,15 @@ class nginx {
   file { '/etc/nginx/nginx.conf':
     ensure => file,
     source => 'puppet:///modules/nginx/nginx.conf'
+    require => Package['nginx']
+    notify => Service['nginx']
   }
   
    file { '/etc/nginx/conf.d/default.conf':
     ensure => file,
     source => 'puppet:///modules/nginx/default.conf'
+    require => Package['nginx']
+    notify => Service['nginx']
   }
   
   service { 'nginx':
