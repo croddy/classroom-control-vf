ba22837bade5e7316d41b36121bd4028329eacae
nestlerw <bill@learningtreelabs.com>
2017-06-21 18:01:56 -0500

    Create managed_user.pp

---
+++ site/users/manifests/managed_user.pp
@@ -0,0 +1,15 @@
+class users::managed_user( $group = $title )
+{
+  user {$title:ensure => present,}
+  file {"/home/${title}":
+    ensure => directory,
+    owner => $title,
+    group => $group,
+  }
+  file {"/home/${title}/.ssh":
+    ensure => directory,
+    owner => $title,
+    group => $group,
+    mode => '0700',
+  }
+}
