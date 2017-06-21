2bb9490b826e8cc3e586eb4b0b4f524a499281a8
nestlerw <bill@learningtreelabs.com>
2017-06-21 13:41:19 -0500

    Class memcached.

--- site/memcached/manifests/init.pp
+++ site/memcached/manifests/init.pp
@@ -1,13 +1,15 @@
-package { 'memcached':
-  ensure => present,
-}
-file { '/etc/sysconfig/memcached':
-  ensure => file,
-  source => 'puppet:///modules/memcached/memcached',
-  require => Package['memcached'],
-}
-service { 'memcached':
-  ensure => running,
-  enable => true,
-  subscribe => File['/etc/sysconfig/memcached'],
+class memcached {
+  package { 'memcached':
+    ensure => present,
+  }
+  file { '/etc/sysconfig/memcached':
+    ensure => file,
+    source => 'puppet:///modules/memcached/memcached',
+    require => Package['memcached'],
+  }
+  service { 'memcached':
+    ensure => running,
+    enable => true,
+    subscribe => File['/etc/sysconfig/memcached'],
+  }
 }
