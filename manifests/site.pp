320d3eeb766ec1c45ef507b87fdecb4a15077e74
burlug <burlug07@gmail.com>
2017-06-21 13:11:02 -0400

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -38,20 +38,3 @@ ini_setting { 'random ordering':
 # will be included in every node's catalog, *in addition* to any classes
 # specified in the console for that node.
 
-node default {
-  # This is where you can declare classes for all nodes.
-  # Example:
-  #   class { 'my_class': }
-  include role::classroom
-  include users
-  include nginx
- 
-}
-
-  
-  exec {'generate motd':
-    command => "cowsay 'Welcome to ${::fqdn}!'> /etc/motd",
-    path => '/usr/local/bin',
-    creates => '/etc/motd',
-    }
-
