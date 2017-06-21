3f66d74e26f765badaa602c8e0e132a05d6c1584
be1823 <betherton@humana.com>
2017-06-21 09:30:45 -0400

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -43,4 +43,8 @@ node default {
   # Example:
   #   class { 'my_class': }
   include role::classroom
+  
+  exec { "cosway 'Welcome to ${::fqdn}!' > /etc/motd":
+    creates  => '/etc/motd',
+  }
 }
