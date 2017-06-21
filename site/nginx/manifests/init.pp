7e1179b3ab05918478687924f410b9d018a89f0f
Oluwayomi Joseph <yomlinux@gmail.com>
2017-06-21 15:15:03 -0400

    Update init.pp

--- site/nginx/manifests/init.pp
+++ site/nginx/manifests/init.pp
@@ -1,25 +1,36 @@
 class nginx {
+  File {
+    owner => 'root',
+    group => 'root',
+    mode => '0644',
+ }
   package { 'nginx':   
    ensure => present,
 }
 file { '/etc/nginx/nginx.conf':
   ensure => file,
   source => 'puppet:///modules/nginx/default.conf',
+  require => package['nginx'],
 }
 file { '/etc/nginx/conf.d/default.conf':
  ensure => file,
  source => 'puppet:///modules/nginx/default.conf',
+ require => package['nginx'],
 }
 service { 'nginx':
  ensure => running,
  enable => true,
+ subscribe => [
+   file['/etc/nginx/nginx.conf'],
+   file['/etc/nginx/conf.d/default.conf'],
+   ]
 }
-file { '/var/www/index.html':
- ensure => file,
- source => 'puppet:///modules/nginx/index.html',
-}
+
 file { '/var/www':
  ensure => directory,
+ 
+file { '/var/www/index.html':
+ ensure => file,
 }
   }
 
