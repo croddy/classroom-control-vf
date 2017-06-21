6b3d27cdc0436b32c7d2df624b4db540e54bfef2
be1823 <betherton@humana.com>
2017-06-21 11:03:10 -0400

    Create init.pp

---
+++ site/skeleton/manifests/init.pp
@@ -0,0 +1,16 @@
+class skelton {
+  file { /etc/skel :
+    ensure => directory,
+    mode   => '0755',
+    owner  => 'root',
+    group  => 'root',
+  }
+  
+  file { /etc/skel/.bashrc :
+    ensure => present,
+    owner  => 'root',
+    group  => 'root',
+    mode   => '0644',
+    source => 'puppet:///modules/skeleton/bashrc',
+  }
+}
