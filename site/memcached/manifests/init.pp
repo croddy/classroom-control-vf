f50ede90f0ccea294d0fbb41f1b1eb161c48fb32
Edward Sechrest <esechres@users.noreply.github.com>
2017-06-21 13:58:00 -0400

    Update init.pp

--- site/memcached/manifests/init.pp
+++ site/memcached/manifests/init.pp
@@ -2,17 +2,18 @@ class memcached {
   package { 'memcached':
     ensure => present,
   }
-  file { '/etc/sysconfig/memcached':
+  file { 'memcached config':
     ensure  => file,
     owner   => 'root',
     group   => 'root',
     mode    => '0644',
+    path => '/etc/sysconfig/memcached',
     source  => 'puppet:///modules/memcached/sysconfig',
     require => Package[;memcached'],
   }
   service { 'memcached':
     ensure    => running,
     enable    => true,
-    subscribe => File['/etc/sysconfig/memcached'],
+    subscribe => File['memcached config'],
   }
 }
