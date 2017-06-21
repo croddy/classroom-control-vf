890fc9f2f9c1594106ed83f21427dd45f3e315de
Chris Roddy <cmroddy@gmail.com>
2017-06-21 15:38:42 -0400

    add a conditional notify

--- manifests/site.pp
+++ manifests/site.pp
@@ -38,5 +38,7 @@ ini_setting { 'random ordering':
 # will be included in every node's catalog, *in addition* to any classes
 # specified in the console for that node.
 
-
+if $is_virtual {
+  notify { "This is a ${virtual} system.": }
+}
 
