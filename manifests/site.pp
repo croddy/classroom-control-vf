9bcc4197e2f25aa9a4ed7eed23f4bed5c4bb4b67
Edward Sechrest <esechres@users.noreply.github.com>
2017-06-21 15:41:07 -0400

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -37,3 +37,10 @@ ini_setting { 'random ordering':
 # definition. If there are no other nodes in this file, classes declared here
 # will be included in every node's catalog, *in addition* to any classes
 # specified in the console for that node.
+
+node default {
+  if $::virtual != 'physical' {
+    $vmname = capitalize($::virtual)
+    notify { "This is a ${vmname} virtual machine.": }
+  }
+}
