374a375ccc624816e06f195f84135638b17056d3
moberlander08 <moberlander08@gmail.com>
2017-06-21 14:25:48 -0400

    Update init.pp

--- site/nginx/manifests/init.pp
+++ site/nginx/manifests/init.pp
@@ -6,21 +6,25 @@ class nginx {
   file { '/etc/nginx/nginx.conf':
     ensure => file,
     source => 'puppet:///modules/nginx/nginx.conf',
+    require => Package['nginx'],
   }
   
   file { '/etc/nginx/config.d/default.conf':
     ensure => file,
     source => 'puppet:///modules/nginx/default.conf',
+    require => Package['nginx'],
   }
   
   file { '/usr/share/nginx/html/index.html':
     ensure => file,
     source => 'puppet:///modules/nginx/index.html',
+    require => Package['nginx'],    
   }
   
   service { 'nginx':
     ensure => running,
     enable => true,
+    subscribe => File['/etc/nginx/config.d/default.conf'],
   }
   
   
