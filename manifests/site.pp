a903d0ccf7802fad881ad097bdd07370adcd8a78
chadeckles <eckles_chad@yahoo.com>
2017-06-21 07:38:05 -0600

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -62,7 +62,7 @@ node default {
   exec { 'generate motd':
     command => 'cowsay "Welcome to ${::fqdn}!" > /etc/motd',
     path => '/usr/local/bin',
-    creates => '/etc/motd'.
+    creates => '/etc/motd',
    }
 
 }
