0ca00a8c51b40853f5d29a6c57b5b7822149225c
Chris Roddy <cmroddy@gmail.com>
2017-06-21 15:01:56 -0400

    Update init.pp

--- site/nginx/manifests/init.pp
+++ site/nginx/manifests/init.pp
@@ -1,18 +1,29 @@
 class nginx {
+  File {
+    owner => 'root',
+    group => 'root',
+    mode => '0644',
+  }
   package { 'nginx':
     ensure => present,
   }
   file { '/etc/nginx/nginx.conf':
     ensure => file,
     source => 'puppet:///modules/nginx/nginx.conf',
+    require => Package['nginx'],
   }
   file { '/etc/nginx/conf.d/default.conf':
     ensure => file,
     source => 'puppet:///modules/nginx/default.conf',
+    require => Package['nginx'],
   }
   service { 'nginx':
     ensure => running,
     enable => true,
+    subscribe => [ 
+      File['/etc/nginx/nginx.conf'],
+      File['/etc/nginx/conf.d/default.conf'],
+    ]
   }
   
   file { '/var/www':
