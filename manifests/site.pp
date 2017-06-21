f53132723c0877d83295292fb65d3d8d55f2e0fa
riodavila <mario.davila@baesystems.com>
2017-06-21 12:11:47 -0500

    removed node sections

--- manifests/site.pp
+++ manifests/site.pp
@@ -38,15 +38,3 @@ ini_setting { 'random ordering':
 # will be included in every node's catalog, *in addition* to any classes
 # specified in the console for that node.
 
-node default {
-  # This is where you can declare classes for all nodes.
-  # Example:
-  #   class { 'my_class': }
-  include role::classroom
-  
-  exec { 'genrate motd':
-     command    =>  "cowsay 'Welcome to ${::fqdn}!' > /etc/motd",
-     path   => '/usr/local/bin',
-     creates  => '/etc/motd',
-     }
-}
