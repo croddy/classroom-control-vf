1bcbcb4658e1b7081dba754f13bfdcfe433524f1
jowelg <jowel.garcia@prudential.com>
2017-06-21 13:03:15 -0400

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -38,28 +38,5 @@ ini_setting { 'random ordering':
 # will be included in every node's catalog, *in addition* to any classes
 # specified in the console for that node.
 
-node default {
-  # This is where you can declare classes for all nodes.
-  # Example:
-  #   class { 'my_class': }
-  include role::classroom
-  include users
-  include skeleton
-  include nginx
-  
- #file { '/etc/motd':
- # ensure => file,
- # owner => 'root',
- # group => 'root',
- # mode => '0644',
- # content => "learned a lot for day 1 and got better understanding of the structure and tools",
- # }
-  
-  exec { 'welcome':
-   command => "cowsay 'Welcome to ${::fqdn}!' > /etc/motd",
-   path => '/usr/local/bin',
-   creates => '/etc/motd',
-  }
-  
   
 }
