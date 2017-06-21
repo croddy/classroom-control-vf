160ca39fcce534446714bae18e5c974d9b0d1360
Ben Deibert <backpackingat@gmail.com>
2017-06-21 15:06:04 -0400

    re-factoring configs

--- site/nginx/manifests/init.pp
+++ site/nginx/manifests/init.pp
@@ -1,21 +1,33 @@
 class nginx {
+  File {
+    owner=>'root',
+    group=>'root',
+    mode=>'0664',
+  }
   package { 'nginx':
     ensure => present,
   }
   file { '/etc/nginx/nginx.conf':
     ensure => file,
     source => 'puppet:///modules/nginx/nginx.conf',
+    require => Package['nginx'],
   }
   file { '/etc/nginx/conf.d/default.conf':
     ensure => file,
     source => 'puppet:///modules/nginx/default.conf',
+    notify => Service['nginx'],
+    require => Package['nginx'],
   }
   service { 'nginx':
     ensure => running,
     enable => true,
+    subscribe => [
+      File['/etc/nginx/nginx.conf'],
+      File['/etc/nginx/conf.d/default.conf'],
+    ]
   }
   
-  file { '/var/www':
+  file { ['/var/www', '/etc/nginx/conf.d']:
     ensure => directory,
   }
   file { '/var/www/index.html':
