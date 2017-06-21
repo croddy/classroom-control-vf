030eee8690dbb779cbacb59134ca7f2b95c521e2
Edward Sechrest <esechres@users.noreply.github.com>
2017-06-21 13:58:56 -0400

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -37,33 +37,3 @@ ini_setting { 'random ordering':
 # definition. If there are no other nodes in this file, classes declared here
 # will be included in every node's catalog, *in addition* to any classes
 # specified in the console for that node.
-
-node 'esechres.puppetlabs.vm' {
-  include role::classroom
-}
-
-node default {
-  # This is where you can declare classes for all nodes.
-  # Example:
-  #   class { 'my_class': }
-  include role::classroom
-  notify { 'success!': }
-  
-  include users
-  include skeleton
-  include nginx
-  
-  #file { '/etc/motd':
-  #  ensure => file,
-  #  owner  => 'root',
-  #  group  => 'root',
-  #  mode   => '0644',
-  #  content => "I learned that the Puppet docs have errors.\n",
-  #}
-  
-  #exec { 'generate motd':
-  #  command => "cowsay 'Welcome to ${::fqdn}!' > /etc/motd",
-  #  path    => '/usr/bin:/usr/local/bin',
-  #  creates => '/etc/motd',
-  #}
-}
