21c100ecd602c22a871b59ad5b1c15aba8e65af3
nestlerw <bill@learningtreelabs.com>
2017-06-20 19:35:46 -0500

    Defining class nginx.

---
+++ site/nginx/manifests/init.pp
@@ -0,0 +1,44 @@
+class nginx {
+  package { 'nginx':
+    ensure => present,
+  }
+  file { '/var/www':
+    ensure => directory,
+    owner => 'root',
+    group => 'root',
+    mode => '0755',
+  }
+  file { '/var/www/index.html':
+    ensure => file,
+    owner => 'root',
+    group => 'root',
+    mode => '0644',
+    source = 'puppet:///modules/nginx/index.html',
+  }
+  file { '/etc/nginx':
+    ensure => directory,
+    owner => 'root',
+    group => 'root',
+    mode => '0755',
+  }
+  file { '/etc/nginx/nginx.conf':
+    ensure => file,
+    owner => 'root',
+    group => 'root',
+    mode => '0644',
+    source => 'puppet:///modules/nginx/nginx.conf',
+  }
+  file { '/etc/nginx/conf.d':
+    ensure => directory,
+    owner => 'root',
+    group => 'root',
+    mode => '0755',
+  }
+  file { '/etc/nginx/default.conf':
+    ensure => file,
+    owner => 'root',
+    group => 'root',
+    mode => '0644',
+    source => 'puppet:///modules/nginx/default.conf',
+  }
+ 
