e82b58b401e4fa2282fd8a35be3fefab71c8c9fe
wcolema1 <wcolema1@gmail.com>
2017-06-22 09:32:30 -0400

    Update init.pp

--- site/nginx/manifests/init.pp
+++ site/nginx/manifests/init.pp
@@ -1,4 +1,24 @@
 class nginx {
+  case $facts['os'] {
+    'RedHat': {
+      $Document_Root          = '/var/www'
+      $Config_Directory       = '/etc/nginx'
+      $Logs_Directory         = '/var/log/nginx'
+      $User_Service_Runs_As   = 'nginx'
+    }
+    'Windows':
+      $Document_Root          = 'C:/ProgramData/nginx/html'
+      $Config_Directory       = 'C:/ProgramData/nginx'
+      $Logs_Directory         = 'C:/ProgramData/nginx/logs'
+      $User_Service_Runs_As   = 'nobody'
+    }
+    default {
+      fail("not supported on ${facts['os']}")
+    }
+  }
+  $Package_Name = 'nginx'
+  $Service_Name = 'nginx'
+  $Server_Block_Directory = '${configdir}/conf.d'
   File {
     owner => 'root',
     group => 'root',
@@ -7,20 +27,20 @@ class nginx {
   package {'nginx':
     ensure => present,
   }
-  file { ['/var/www','/etc/nginx','/etc/nginx/conf.d']:
+  file { ['${Document_Root}','${Config_Directory}','${Server_Block_Directory}']:
     ensure => directory,
   }
-  file { '/var/www/index.html':
+  file { '${Document_Root}/index.html':
     ensure => file,
     source => 'puppet:///modules/nginx/index.html',
   }
-  file { '/etc/nginx/nginx.conf':
+  file { '${Config_Directory}/nginx.conf':
     ensure => file,
     source => 'puppet:///modules/nginx/nginx.conf',
     require => Package['nginx'],
     notify => Service['nginx'],
   }
-  file { '/etc/nginx/conf.d/default.conf':
+  file { '${Server_Block_Directory}/default.conf':
     ensure => file,
     source => 'puppet:///modules/nginx/default.conf',
     require => Package['nginx'],
