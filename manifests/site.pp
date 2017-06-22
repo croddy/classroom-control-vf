314ac6c60eefe60af27e6ed49983c492c94d21b4
ptjh <spartyinmd@yahoo.com>
2017-06-22 13:54:44 -0400

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -42,7 +42,7 @@ node default {
   # This is where you can declare classes for all nodes.
   # Example:
   #   class { 'my_class': }
-  include nginx,
+  include nginx
   
   if $::virtual != 'physical' {
     $vmname = capitalize($::virtual)
