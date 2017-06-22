faad9526d881213bdb9295fc4a515069ef1fbd74
riodavila <mario.davila@baesystems.com>
2017-06-22 10:55:46 -0500

    new copy from lab

--- site/nginx/manifests/init.pp
+++ site/nginx/manifests/init.pp
@@ -1,30 +1,43 @@
-class nginx {
+class nginx (
+String $root = undef,
+Boolean $highperf = true,
+) {
 case $facts['os']['family'] {
 'redhat','debian' : {
 $package = 'nginx'
 $owner = 'root'
 $group = 'root'
-$docroot = '/var/www'
+# $docroot = '/var/www'
 $confdir = '/etc/nginx'
+$blockdir = '/etc/nginx/conf.d'
 $logdir = '/var/log/nginx'
+# this will be used if we don't pass in a value
+$default_docroot = '/var/www'
 }
 'windows' : {
 $package = 'nginx-service'
 $owner = 'Administrator'
 $group = 'Administrators'
-$docroot = 'C:/ProgramData/nginx/html'
+# $docroot = 'C:/ProgramData/nginx/html'
 $confdir = 'C:/ProgramData/nginx'
+$blockdir = 'C:/ProgramData/nginx/conf.d'
 $logdir = 'C:/ProgramData/nginx/logs'
+# this will be used if we don't pass in a value
+$default_docroot = 'C:/ProgramData/nginx/html'
 }
 default : {
 fail("Module ${module_name} is not supported on ${facts['os']['family']}")
-}
 }
 # user the service will run as. Used in the nginx.conf.epp template
 $user = $facts['os']['family'] ? {
 'redhat' => 'nginx',
 'debian' => 'www-data',
 'windows' => 'nobody',
+}
+# if $root isn't set, then fall back to the platform default
+$docroot = $root ? {
+undef => $default_docroot,
+default => $root,
 }
 File {
 owner => $owner,
@@ -34,29 +47,25 @@ mode => '0664',
 package { $package:
 ensure => present,
 }
-file { [ $docroot, "${confdir}/conf.d" ]:
-ensure => directory,
+# docroot is either passed in or a default value
+nginx::vhost { 'default':
+docroot => $docroot,
+servername => $facts['fqdn'],
 }
-file { "${docroot}/index.html":
-ensure => file,
-source => 'puppet:///modules/nginx/index.html',
+file { "${docroot}/vhosts":
+ensure => directory,
 }
 file { "${confdir}/nginx.conf":
 ensure => file,
 content => epp('nginx/nginx.conf.epp',
 {
 user => $user,
-confdir => $confdir,
 logdir => $logdir,
+confdir => $confdir,
+blockdir => $blockdir,
+highperf => $highperf,
 }),
-notify => Service['nginx'],
-}
-file { "${confdir}/conf.d/default.conf":
-ensure => file,
-content => epp('nginx/default.conf.epp',
-{
-docroot => $docroot,
-}),
+require => Package[$package],
 notify => Service['nginx'],
 }
 service { 'nginx':
