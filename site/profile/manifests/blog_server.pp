ece2063159536d447c252ed372e0ae7930a33996
Chris Roddy <cmroddy@gmail.com>
2017-06-22 15:59:56 -0400

    add a user account

--- site/profile/manifests/blog_server.pp
+++ site/profile/manifests/blog_server.pp
@@ -1,5 +1,11 @@
 class profile::blog_server {
   class { 'mysql::server':
-    root_password => 'strong_password1' # note: not a very strong password
+    root_password => 'strong_password1', # note: not a very strong password
+    users => {
+      'wordpress@localhost' => {
+        ensure => 'present',
+        password_hash => '*34FD07B148CCA9FCB2BE6C2EA20689053E748B13',
+      },
+    },
   }
 }
