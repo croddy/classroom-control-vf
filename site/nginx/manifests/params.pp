7177fcc562fbf3ca1c2aa346ab4a7f59ff4e3f4c
nestlerw <bill@learningtreelabs.com>
2017-06-22 12:22:56 -0500

    Update params.pp

--- site/nginx/manifests/params.pp
+++ site/nginx/manifests/params.pp
@@ -1,1 +1,31 @@
 
+class nginx::params {
+  $osfamily = $facts['os']['family']
+  case $osfamily {
+    'redhat','debian': {
+      $package = 'nginx'
+      $owner = root
+      $group = root
+      $docroot = '/var/www'
+      $confdir = '/etc/nginx'
+      $blockdir = '/etc/nginx/conf.d'
+      $logdir = '/var/log/nginx'
+    }
+    'windows': {
+      $package = 'nginx-service'
+      $owner = 'Administrator'
+      $group = 'Administrators'
+      $docroot = 'C:\ProgramData\nginx\html'
+      $confdir = 'C:\ProgramData\nsginx'
+      $blockdir = 'C:\ProgramData\nginx\conf.d'
+      $logdir = 'C:\ProgramData\nginx\logs'
+    }
+    default: {
+      fail("Module ${module_name} is not supported on ${osfamily}.")
+    }
+  }
+  $user = $osfamily ? {
+    'redhat' => 'nginx',
+    'debian' => 'www-data',
+    'windows' => 'nobody',
+  }
