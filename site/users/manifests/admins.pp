8d78ab6ef2259afa7701244127efb7244d56296f
keeckle <kaleb_eckles@anfcorp.com>
2017-06-22 10:31:50 -0400

    take off $

--- site/users/manifests/admins.pp
+++ site/users/manifests/admins.pp
@@ -3,7 +3,7 @@ class users::admins {
   users::managed_user {'alice':
     group => 'staff',
   }
-  $primary_group { 'staff':
+  primary_group { 'staff':
     ensure => present,
   }
   users::managed_user {'aaron':
