a2868b16e34f2fe1186d3fb837b8b6353ec1a7d9
nestlerw <bill@learningtreelabs.com>
2017-06-22 16:00:46 -0500

    Update init.pp

--- site/nginx/manifests/init.pp
+++ site/nginx/manifests/init.pp
@@ -18,8 +18,16 @@ class nginx
   }
 
   $service_name = 'nginx'
-  $port = '80'
   
+  $template_params = {
+    user => $user,
+    blockdir => $blockdir,
+    confdir => $confdir,
+    docroot => $docroot,
+    logdir => $logdir,
+    port => '80',
+  }
+
   package { $package:
     ensure => present,
   }  
@@ -30,19 +38,12 @@ class nginx
     source => 'puppet:///modules/nginx/index.html',
   }
   file { "${confdir}/nginx.conf":
-    content => epp('nginx/nginx.conf.epp',
-      {
-        user => $user,
-        blockdir => $blockdir,
-        confdir => $confdir,
-        logdir => $logdir,
-      }
-    ),
+    content => epp('nginx/nginx.conf.epp',$template_params),
     require => Package[$package],
     notify => Service[$service_name],
   }
   file { "${blockdir}/default.conf":
-    content => epp('nginx/default.conf.epp',{docroot => $docroot, port => $port }),
+    content => epp('nginx/default.conf.epp',$template_params),
     require => Package[$package],
     notify => Service[$service_name],
   }
