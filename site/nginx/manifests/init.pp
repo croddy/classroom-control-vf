ea190843182f3f5d4b76b592af9a2a8dd388cae2
ptjh <spartyinmd@yahoo.com>
2017-06-22 12:11:51 -0400

    Update init.pp

--- site/nginx/manifests/init.pp
+++ site/nginx/manifests/init.pp
@@ -1,85 +1,85 @@
-class nginx {
-     String $root = undef,
-     Boolean $highperf = true,
+class nginx (
+  $root = undef,
 ) {
-
-     case $facts['os']['family'] {
-       'redhat','debian' : {
-         $package = 'nginx'
-         $owner = 'root'
-         $group = 'root'
-#         $docroot = '/var/www'
-         $confdir = '/etc/nginx'
-         $blockdir = '/etc/nginx/conf.d'
-         $logdir = '/var/log/nginx'
-         
-         $default_docroot = 'var/www'
-      }
-      'windows' : {
-         $package = 'nginx-service'
-         $owner = 'Administrator'
-         $group = 'Administrators'
-#         $docroot = 'C:/ProgramData/nginx/html'
-         $confdir = 'C:/ProgramData/nginx'
-         $blockdir = 'C:/ProgramData/nginx/conf.d'
-         $logdir = 'C:/ProgramData/nginx/logs'
-         
-         $default_docrott = 'C:/ProgramData/nginx/html'
-      }
-      default : {
-         fail("Module ${module_name} is not supported on ${facts['os']['family']}")
-      }
+  case $osfamily {
+    'redhat': {
+      $package = 'nginx'
+      $owner = 'root'
+      $group = 'root'
+      $docroot = '/var/www'
+      $confdir = '/etc/nginx'
+      $blockdir = '/etc/nginx/conf.d'
+      $logdir = '/var/log/nginx'
+      $service = 'nginx'
+      $user = 'nginx'
     }
-
-# user the service will run as. Used in the nginx.conf.epp template
- $user = $facts['os']['family'] ? {
-   'redhat' => 'nginx',
-   'debian' => 'www-data',
-   'windows' => 'nobody',
- }
-
-# if $root isn't set, then fall back to the platform default
- $docroot = $root ? {
-   undef => $default_docroot,
-   default => $root,
- }
-
- File {
-   owner => $owner,
-   group => $group,
-   mode => '0664',
- }
-
- package { $package:
-   ensure => present,
- }
-
-# docroot is either passed in or a default value
- nginx::vhost { 'default':
-   docroot => $docroot,
-   servername => $facts['fqdn'],
- }
-
- file { "${docroot}/vhosts":
-    ensure => directory,
- }
-
- file { "${confdir}/nginx.conf":
+    'debian': {
+      $package = 'nginx'
+      $owner = 'root'
+      $group = 'root'
+      $docroot = '/var/www'
+      $confdir = '/etc/nginx'
+      $blockdir = '/etc/nginx/conf.d'
+      $logdir = '/var/log/nginx'
+      $service = 'nginx'
+      $user = 'www-data'
+    }
+    'windows': {
+      $package = 'nginx-service'
+      $owner = 'Administrator'
+      $group = 'Administrators'
+      $docroot = 'C:/ProgramData/nginx/html'
+      $confdir = 'C:/ProgramData/nginx'
+      $blockdir = 'C:/ProgramData/nginx/conf.d'
+      $logdir = 'C:/ProgramData/nginx/logs'
+      $service = 'nginx'
+      $user = 'nobody'
+    }
+    default: { fail("Unsupported platform: ${::osfamily}") }
+  }
+  $real_docroot = pick($root, $docroot)
+  
+  $template_params = {
+    docroot => $real_docroot,
+    confdir => $confdir,
+    blockdir => $blockdir,
+    logdir => $logdir,
+    user => $user,
+  }
+  File {
+    owner => $owner,
+    group => $group,
+    mode => '0644',
+  }
+  package { $package:
+    ensure => present,
+  }
+  file { 'nginx main config':
     ensure => file,
-    content => epp('nginx/nginx.conf.epp',
-             {
-              user => $user,
-              logdir => $logdir,
-              confdir => $confdir,
-              blockdir => $blockdir,
-              highperf => $highperf,
-             }),
-   require => Package[$package],
-   notify => Service['nginx'],
- }
- 
- service { 'nginx':
-   ensure => running,
-   enable => true,
- }
+    path => "${confdir}/nginx.conf",
+    content => epp('nginx/nginx.conf.epp', $template_params),
+    require => Package[$package],
+  }
+  file { 'nginx default block':
+    ensure => file,
+    path => "${blockdir}/default.conf",
+    content => epp('nginx/default.conf.epp', $template_params),
+    require => Package[$package],
+  }
+  service { 'nginx':
+    ensure => running,
+    enable => true,
+    subscribe => [ 
+      File['nginx main config'],
+      File['nginx default block'],
+    ]
+  }
+  
+  file { $real_docroot:
+    ensure => directory,
+  }
+  file { "${real_docroot}/index.html":
+    ensure => file,
+    source => 'puppet:///modules/nginx/index.html',
+  }
 }
