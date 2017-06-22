302034ff29c9dea2d202d9a9f68345f8cc5e8f63
ptjh <spartyinmd@yahoo.com>
2017-06-22 12:43:09 -0400

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -43,6 +43,10 @@ node default {
   # Example:
   #   class { 'my_class': }
   
+  class { 'nginx':
+     root => '/var/www/html',
+  }
+
   if $::virtual != 'physical' {
     $vmname = capitalize($::virtual)
     notify { "This is a ${vmname} virtual machine.": }
