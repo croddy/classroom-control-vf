e788be2fa86bc4f1ab1ce76356f38d69d04c9540
nestlerw <bill@learningtreelabs.com>
2017-06-22 09:35:05 -0500

    Update managed_user.pp

--- site/users/manifests/managed_user.pp
+++ site/users/manifests/managed_user.pp
@@ -1,4 +1,4 @@
-define users::managed_user( $user = $title, $group = $title, $home = "/home/${title}" )
+define users::managed_user( $user = $title, $group = $title, $home = "/home/${title}", $mode = '0644' )
 {
   user {$user: ensure => present}
   file { "${user} home":
@@ -6,20 +6,21 @@ define users::managed_user( $user = $title, $group = $title, $home = "/home/${ti
     ensure => directory,
     owner => $user,
     group => $group,
+    mode => $mode,
   }
   file { "${user} bashrc":
     path => "${home}/.bashrc",
     ensure => file,
     owner => $user,
     group => $group,
-    mode => '0644',
+    mode => $mode,
   }
   file { "${user} bash_profile":
     path => "${home}/.bash_profile",
     ensure => file,
     owner => $user,
     group => $group,
-    mode => '0644',
+    mode => $mode,
   }
   file { "${user} sshdir":
     path => "${home}/.ssh",
