cb3bf0f0e8f37f2ddeeb394c06fe47a3524e259c
Oluwayomi Joseph <yomlinux@gmail.com>
2017-06-22 09:19:21 -0400

    nginix

--- site/nginx/manifests/init.pp
+++ site/nginx/manifests/init.pp
@@ -3,35 +3,31 @@ class nginx {
     owner => 'root',
     group => 'root',
     mode => '0644',
- }
-  package { 'nginx':   
-   ensure => present,
+  }
+  package {'nginx':
+    ensure => present,
+  }
+  file { ['/var/www','/etc/nginx','/etc/nginx/conf.d']:
+    ensure => directory,
+  }
+  file { '/var/www/index.html':
+    ensure => file,
+    source => 'puppet:///modules/nginx/index.html',
+  }
+  file { '/etc/nginx/nginx.conf':
+    ensure => file,
+    source => 'puppet:///modules/nginx/nginx.conf',
+    require => Package['nginx'],
+    notify => Service['nginx'],
+  }
+  file { '/etc/nginx/conf.d/default.conf':
+    ensure => file,
+    source => 'puppet:///modules/nginx/default.conf',
+    require => Package['nginx'],
+    notify => Service['nginx'],
+  }
+  service { 'nginx':
+    ensure => running,
+    enable => true,
+  }
 }
-file { '/etc/nginx/nginx.conf':
-  ensure => file,
-  source => 'puppet:///modules/nginx/default.conf',
-  require => package['nginx'],
-}
-file { '/etc/nginx/conf.d/default.conf':
- ensure => file,
- source => 'puppet:///modules/nginx/default.conf',
- require => package['nginx'],
-}
-service { 'nginx':
- ensure => running,
- enable => true,
- subscribe => [
-   file['/etc/nginx/nginx.conf'],
-   file['/etc/nginx/conf.d/default.conf'],
-   ]
-}
-
-file { '/var/www':
- ensure => directory,
- }
-file { '/var/www/index.html':
- ensure => file,
- source => 'puppet:///modules/nginx/index.html',
-    }
-}
-
