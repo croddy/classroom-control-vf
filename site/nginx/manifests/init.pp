dce035009927a2957bc201ef1dc7b1b9d3e554bf
ptjh <spartyinmd@yahoo.com>
2017-06-22 12:53:56 -0400

    Update init.pp

--- site/nginx/manifests/init.pp
+++ site/nginx/manifests/init.pp
@@ -1,79 +1,85 @@
 class nginx (
-   $root = undef,
-) {  
-   case $facts['os']['family'] {
-      'redhat','debian' : {
-        $package = 'nginx'
-        $owner = 'root'
-        $group = 'root'
-        $docroot = '/var/www'
-        $confdir = '/etc/nginx'
-        $logdir = '/var/log/nginx'
-      }
-      'windows' : {
-        $package = 'nginx-service'
-        $owner = 'Administrator'
-        $group = 'Administrators'
-        $docroot = 'C:/ProgramData/nginx/html'
-        $confdir = 'C:/ProgramData/nginx'
-        $logdir = 'C:/ProgramData/nginx/logs'
-      }
-      default : {
-          fail("Module ${module_name} is not supported on ${facts['os']['family']}")
-      }
-   }
-   
-   $real_docroot = pick($root, $docroot)
-   
-   $user = $facts['os']['family'] ? {
-     'redhat' => 'nginx',
-     'debian' => 'www-data',
-     'windows' => 'nobody',
-   }
-   
-   File {
-      owner => $owner,
-      group => $group,
-      mode => '0664',
-   }
-   
-   package { $package:
-      ensure => present,
-   }
-
-   file { [ $real_docroot, "${confdir}/conf.d" ]:
-      ensure => directory,
-   }
-
-   file { "${real_docroot}/index.html":
-      ensure => file,
-      source => 'puppet:///modules/nginx/index.html',
-   }
-
-   file { "${confdir}/nginx.conf":
-      ensure => file,
-      content => epp('nginx/nginx.conf.epp',
-         {
-             user => $user,
-             confdir => $confdir,
-             logdir => $logdir,
-         }),
-
-      notify => Service['nginx'],
-   }
-   
-   file { "${confdir}/conf.d/default.conf":
-      ensure => file,
-      content => epp('nginx/default.conf.epp',
-         {
-             docroot => $real_docroot,
-         }),
-
-      notify => Service['nginx'],
-   }
-
-   service { 'nginx':
-      ensure => running,
-      enable => true,
-   }
+  $root = undef,
+) {
+  case $osfamily {
+    'redhat': {
+      $package = 'nginx'
+      $owner = 'root'
+      $group = 'root'
+      $docroot = '/var/www'
+      $confdir = '/etc/nginx'
+      $blockdir = '/etc/nginx/conf.d'
+      $logdir = '/var/log/nginx'
+      $service = 'nginx'
+      $user = 'nginx'
+    }
+    'debian': {
+      $package = 'nginx'
+      $owner = 'root'
+      $group = 'root'
+      $docroot = '/var/www'
+      $confdir = '/etc/nginx'
+      $blockdir = '/etc/nginx/conf.d'
+      $logdir = '/var/log/nginx'
+      $service = 'nginx'
+      $user = 'www-data'
+    }
+    'windows': {
+      $package = 'nginx-service'
+      $owner = 'Administrator'
+      $group = 'Administrators'
+      $docroot = 'C:/ProgramData/nginx/html'
+      $confdir = 'C:/ProgramData/nginx'
+      $blockdir = 'C:/ProgramData/nginx/conf.d'
+      $logdir = 'C:/ProgramData/nginx/logs'
+      $service = 'nginx'
+      $user = 'nobody'
+    }
+    default: { fail("Unsupported platform: ${::osfamily}") }
+  }
+  $real_docroot = pick($root, $docroot)
+  
+  $template_params = {
+    docroot => $real_docroot,
+    confdir => $confdir,
+    blockdir => $blockdir,
+    logdir => $logdir,
+    user => $user,
+  }
+  File {
+    owner => $owner,
+    group => $group,
+    mode => '0644',
+  }
+  package { $package:
+    ensure => present,
+  }
+  file { 'nginx main config':
+    ensure => file,
+    path => "${confdir}/nginx.conf",
+    content => epp('nginx/nginx.conf.epp', $template_params),
+    require => Package[$package],
+  }
+  file { 'nginx default block':
+    ensure => file,
+    path => "${blockdir}/default.conf",
+    content => epp('nginx/default.conf.epp', $template_params),
+    require => Package[$package],
+  }
+  service { 'nginx':
+    ensure => running,
+    enable => true,
+    subscribe => [ 
+      File['nginx main config'],
+      File['nginx default block'],
+    ]
+  }
+  
+  file { $real_docroot:
+    ensure => directory,
+  }
+  file { "${real_docroot}/index.html":
+    ensure => file,
+    source => 'puppet:///modules/nginx/index.html',
+  }
 }
