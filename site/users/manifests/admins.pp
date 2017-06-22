8f7c32d017f384e986e1d887ed7022fa085d94e3
nestlerw <bill@learningtreelabs.com>
2017-06-22 09:38:13 -0500

    Update admins.pp

--- site/users/manifests/admins.pp
+++ site/users/manifests/admins.pp
@@ -1,5 +1,5 @@
 class users::admins {
-  users::managed_user {'joe': mode => '0600'}
+  users::managed_user {'joe': mode => '0640'}
   users::managed_user {'alice':group => 'staff'}
   users::managed_user {'bill':group => 'staff'}
   group {'staff': ensure => present,}
