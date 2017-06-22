a2ce95fc7a6f0b4570794c5f8b70e297f49923be
keeckle <kaleb_eckles@anfcorp.com>
2017-06-22 09:59:03 -0400

    manage user resources with defined types

---
+++ site/users/manifests/managed_user.pp
@@ -0,0 +1,24 @@
+define users::managed_user (
+  $homedir = "/home/${title}",
+  $primary_group = $title,
+  $mode = "0644",
+) {
+  user { $title:
+    ensure => present,
+    gid => $primary_group,
+  }
+  group { $primary_group:
+    ensure => present,
+  }
+  file { $homedir:
+    owner => $title,
+    group => $primary_group,
+    mode => $mode,
+  }
+  file { "${homedir}/.ssh":
+    ensure => present,
+    owner => $title,
+    group => $primary_group,
+    mode => '0600',
+  }
+}
