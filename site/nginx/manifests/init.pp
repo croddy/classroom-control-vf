bedaff4b93d59f7bda782be4ff444eab63e320d4
mcicello <mcicello@hotmail.com>
2017-06-22 14:30:07 -0400

    Update init.pp

--- site/nginx/manifests/init.pp
+++ site/nginx/manifests/init.pp
@@ -1,71 +1,44 @@
-class nginx(
-String $root = undef,
-Boolean $highperf = true,
-)
-{
-case $facts['os']['family'] {
-'redhat','debian' : {
-          $package = 'nginx'
-          $owner = 'root'
-          $group = 'root'
-          $docroot = '/var/www'
-          $confdir = '/etc/nginx'
-          $logdir = '/var/log/nginx'
-          }
-'windows' : {
-        $package = 'nginx-service'
-        $owner = 'Administrator'
-        $group = 'Administrators'
-        $docroot = 'C:/ProgramData/nginx/html'
-        $confdir = 'C:/ProgramData/nginx'
-        $logdir = 'C:/ProgramData/nginx/logs'
-        }
-default : {
-  fail("Module ${module_name} is not supported on ${facts['os']['family']}")
-  }
-}
-# user the service will run as. Used in the nginx.conf.epp template
-$user = $facts['os']['family'] ? {
-  'redhat' => 'nginx',
-  'debian' => 'www-data',
-  'windows' => 'nobody',
-}
+class nginx (
+String $package = $nginx::params::package,
+String $owner = $nginx::params::owner,
+String $group = $nginx::params::group,
+String $docroot = $nginx::params::docroot,
+String $confdir = $nginx::params::confdir,
+String $blockdir = $nginx::params::blockdir,
+String $logdir = $nginx::params::logdir,
+String $user = $nginx::params::user,
+Boolean $highperf = $nginx::params::highperf,
+) inherits nginx::params {
 File {
-  owner => $owner,
-  group => $group,
-  mode => '0664',
+owner => $owner,
+group => $group,
+mode => '0664',
 }
 package { $package:
-  ensure => present,
+ensure => present,
 }
-file { [ $docroot, "${confdir}/conf.d" ]:
-  ensure => directory,
+nginx::vhost { 'default':
+docroot => $docroot,
+servername => $facts['fqdn'],
 }
-file { "${docroot}/index.html":
-  ensure => file,
-  source => 'puppet:///modules/nginx/index.html',
+file { "${docroot}/vhosts":
+ensure => directory,
 }
 file { "${confdir}/nginx.conf":
-  ensure => file,
-  content => epp('nginx/nginx.conf.epp',
+ensure => file,
+content => epp('nginx/nginx.conf.epp',
 {
-  user => $user,
-  confdir => $confdir,
-  logdir => $logdir,
+user => $user,
+logdir => $logdir,
+confdir => $confdir,
+blockdir => $blockdir,
+highperf => $highperf,
 }),
+require => Package[$package],
 notify => Service['nginx'],
 }
-file { "${confdir}/conf.d/default.conf":
-  ensure => file,
-  content => epp('nginx/default.conf.epp',
-                    {
-                    docroot => $docroot,
-                    }),
-  notify => Service['nginx'],
-}
-
 service { 'nginx':
-  ensure => running,
-  enable => true,
+ensure => running,
+enable => true,
 }
 }
