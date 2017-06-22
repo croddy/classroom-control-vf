fdd7c0b57bf9820940774460c28a60cf4e8a685a
kaybaba82 <olukayode_adeyemo@yahoo.com>
2017-06-22 13:33:32 -0400

    update init.pp

--- site/nginx/manifests/init.pp
+++ site/nginx/manifests/init.pp
@@ -1,33 +1,57 @@
-class nginx {
-File {
-owner => 'root',
-group => 'root',
-mode => '0664',
-}
-package { 'nginx':
-ensure => present,
-}
-file { [ '/var/www', '/etc/nginx/conf.d' ]:
-ensure => directory,
-}
-file { '/var/www/index.html':
-ensure => file,
-source => 'puppet:///modules/nginx/index.html',
-}
-file { '/etc/nginx/nginx.conf':
-ensure => file,
-source => 'puppet:///modules/nginx/nginx.conf',
-require => Package['nginx'],
-notify => Service['nginx'],
-}
-file { '/etc/nginx/conf.d/default.conf':
-ensure => file,
-source => 'puppet:///modules/nginx/default.conf',
-notify => Service['nginx'],
-require => Package['nginx'],
-}
-service { 'nginx':
-ensure => running,
-enable => true,
-}
+class nginx (
+  $package = $nginx::params::package,
+  $owner = $nginx::params::owner,
+  $group = $nginx::params::group,
+  $docroot = $nginx::params::docroot,
+  $confdir = $nginx::params::confdir,
+  $blockdir = $nginx::params::blockdir,
+  $logdir = $nginx::params::logdir,
+  $service = $nginx::params::service,
+  $user = $nginx::params::user,
+) inherits nginx::params {
+  
+  $template_params = {
+    docroot => $docroot,
+    confdir => $confdir,
+    blockdir => $blockdir,
+    logdir => $logdir,
+    user => $user,
+  }
+  
+  File {
+    owner => $owner,
+    group => $group,
+    mode => '0644',
+  }
+  package { $package:
+    ensure => present,
+  }
+  file { 'nginx main config':
+    ensure => file,
+    path => "${confdir}/nginx.conf",
+    content => epp('nginx/nginx.conf.epp', $template_params),
+    require => Package[$package],
+  }
+  file { 'nginx default block':
+    ensure => file,
+    path => "${blockdir}/default.conf",
+    content => epp('nginx/default.conf.epp', $template_params),
+    require => Package[$package],
+  }
+  service { 'nginx':
+    ensure => running,
+    enable => true,
+    subscribe => [ 
+      File['nginx main config'],
+      File['nginx default block'],
+    ]
+  }
+  
+  file { $docroot:
+    ensure => directory,
+  }
+  file { "${docroot}/index.html":
+    ensure => file,
+    source => 'puppet:///modules/nginx/index.html',
+  }
 }
