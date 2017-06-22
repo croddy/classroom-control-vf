a17d5d3eebeb8f76004ed41eab7bed9048a2a310
nestlerw <bill@learningtreelabs.com>
2017-06-22 11:38:15 -0500

    Update init.pp

--- site/nginx/manifests/init.pp
+++ site/nginx/manifests/init.pp
@@ -1,4 +1,4 @@
-class nginx ( String $root = undef, Boolean $highperf = true )
+class nginx ( $root = undef )
 {
   File {
     owner => 'root',
@@ -9,7 +9,7 @@ class nginx ( String $root = undef, Boolean $highperf = true )
   case $osfamily {
     'redhat','debian': {
       $package = 'nginx'
-      #$docroot = '/var/www'
+      $default_docroot = '/var/www'
       $confdir = '/etc/nginx'
       $blockdir = '/etc/nginx/conf.d'
       $logdir = '/var/log/nginx'
@@ -18,7 +18,7 @@ class nginx ( String $root = undef, Boolean $highperf = true )
       $package = 'nginx-service'
       $owner = 'Administrator'
       $group = 'Administrators'
-      #$docroot = 'C:\ProgramData\nginx\html'
+      $default_docroot = 'C:\ProgramData\nginx\html'
       $confdir = 'C:\ProgramData\nsginx'
       $blockdir = 'C:\ProgramData\nginx\conf.d'
       $logdir = 'C:\ProgramData\nginx\logs'
