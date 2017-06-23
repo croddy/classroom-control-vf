b09c3975067fe6176d14d6c58dac96d2db2377a0
moberlander08 <moberlander08@gmail.com>
2017-06-23 19:29:32 -0400

    Update init.pp

--- site/nginx/manifests/init.pp
+++ site/nginx/manifests/init.pp
@@ -1,49 +1,59 @@
-class nginx (
-  String $package = $nginx::params::package,
-  String $owner = $nginx::params::owner,
-  String $group = $nginx::params::group,
-  String $docroot = $nginx::params::docroot,
-  String $confdir = $nginx::params::confdir,
-  String $blockdir = $nginx::params::blockdir,
-  String $logdir = $nginx::params::logdir,
-  String $user = $nginx::params::user,
-  Boolean $highperf = $nginx::params::highperf,
-) inherits nginx::params {
-  File {
-  owner => $owner,
-  group => $group,
-  mode => '0664',
+class nginx {
+case $facts['os']['family'] {
+'redhat','debian' : {
+$package = 'nginx'
+$owner = 'root'
+$group = 'root'
+$docroot = '/var/www'
+$confdir = '/etc/nginx'
+$logdir = '/var/log/nginx'
 }
-
+'windows' : {
+$package = 'nginx-service'
+$owner = 'Administrator'
+$group = 'Administrators'
+$docroot = 'C:/ProgramData/nginx/html'
+$confdir = 'C:/ProgramData/nginx'
+$logdir = 'C:/ProgramData/nginx/logs'
+}
+default : {
+fail("Module ${module_name} is not supported on ${facts['os']['family']}")
+}
+}
+# user the service will run as. Used in the nginx.conf.epp template
+$user = $facts['os']['family'] ? {
+'redhat' => 'nginx',
+'debian' => 'www-data',
+'windows' => 'nobody',
+}
+File {
+owner => $owner,
+group => $group,
+mode => '0664',
+}
 package { $package:
-  ensure => present,
+ensure => present,
 }
-
-#nginx::vhost { 'default':
-#  docroot => $docroot,
-#  servername => $facts['fqdn'],
-#}
-
-file { "${docroot}/vhosts":
-  ensure => directory,
+file { [ "${docroot}/vhosts", "${confdir}/conf.d" ]:
+ensure => directory,
 }
-
+# manage the default docroot, index, and conf--replaces several resources
+nginx::vhost { 'default':
+docroot => $docroot,
+servername => $facts['fqdn'],
+}
 file { "${confdir}/nginx.conf":
-  ensure => file,
-  content => epp('nginx/nginx.conf.epp',
-  {
-    user => $user,
-    logdir => $logdir,
-    confdir => $confdir,
-    blockdir => $blockdir,
-    highperf => $highperf,
-  }),
-  require => Package[$package],
-  notify => Service['nginx'],
+ensure => file,
+content => epp('nginx/nginx.conf.epp',
+{
+user => $user,
+confdir => $confdir,
+logdir => $logdir,
+}),
+notify => Service['nginx'],
 }
-
 service { 'nginx':
-  ensure => running,
-  enable => true,
-  }
+ensure => running,
+enable => true,
+}
 }
